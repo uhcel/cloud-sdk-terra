433abd619e3cd403a4f83f67893e5b8d90477c69
uhcel <leszek.wojnarski@gmail.com>
2022-05-11 15:04:39 +0200

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -1,12 +1,12 @@
 #Cloud Storage
 resource "google_storage_bucket" "image-store-1" {
-  name     = "cf-random-leszek-image-store-bucket-1"
+  name     = "terra-pieceofcloud-001"
   location = "europe-north1"
 }
 
 # GKE cluster
 resource "google_container_cluster" "primary" {
-  name     = "terra-leszek-gke"
+  name     = "terra-pieceofcloud-001"
   location = "europe-north1"
   
   # We can't create a cluster with no node pool defined, but we want to only use
