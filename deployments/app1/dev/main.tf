e90c3a4851d0c5dc0220d430f43160a174622bc4
uhcel <leszek.wojnarski@gmail.com>
2022-05-11 15:15:56 +0200

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -1,17 +1,17 @@
 #Cloud Storage
-resource "google_storage_bucket" "image-store-1" {
+resource "google_storage_bucket" "terra-pieceofcloud-001" {
   name     = "terra-pieceofcloud-001"
   location = "europe-north1"
 }
 
 # GKE cluster
-resource "google_container_cluster" "primary" {
-  name     = "terra-pieceofcloud-001"
-  location = "europe-north1"
+#resource "google_container_cluster" "primary" {
+#  name     = "terra-pieceofcloud-001"
+#  location = "europe-north1"
   
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
-}
+#  remove_default_node_pool = true
+#  initial_node_count       = 1
+#}
