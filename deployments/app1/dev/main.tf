5cf24fa9e8c54abbeda2c61803d92f9522295502
uhcel <leszek.wojnarski@gmail.com>
2022-07-11 11:30:32 +0200

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -1,4 +1,4 @@
-#Google Cloud Storage bucket
+#Google Cloud Storage buckets
 resource "google_storage_bucket" "sb-pieceofcloud-008" {
   name     = "sb-pieceofcloud-008"
   location = "europe-north1"
