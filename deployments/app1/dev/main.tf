088c53e41d3a62ef443ff65ecd682603a4ec4747
uhcel <leszek.wojnarski@gmail.com>
2022-07-19 09:45:07 +0200

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -1,11 +1,12 @@
 #Google Cloud Storage buckets
-resource "google_storage_bucket" "sb-pieceofcloud-008" {
-  name     = "sb-pieceofcloud-010"
+resource "google_storage_bucket" "sbucket-leszek-01" {
+  name     = "sbucket-leszek-01"
   location = "europe-north1"
 }
+#GKE cluster
 resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster"
-  location = "europe-west6"
+  name     = "my-gke-cluster-01"
+  location = "europe-north1"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
