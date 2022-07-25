cbbf897d54eb63159ca961a62ae1e2996c204b31
uhcel <leszek.wojnarski@gmail.com>
2022-07-25 09:31:53 +0200

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -1,5 +1,5 @@
 #Google Cloud Storage buckets
 resource "google_storage_bucket" "sbucket-leszek-01" {
-  name     = "sbucket-leszek-04"
+  name     = "sbucket-leszek-05"
   location = "europe-north1"
 }
