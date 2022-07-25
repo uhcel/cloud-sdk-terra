11711835793733ae115cfe78e4c937d384bf5a17
uhcel <leszek.wojnarski@gmail.com>
2022-07-25 09:10:16 +0200

    Update main.tf

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -3,14 +3,3 @@ resource "google_storage_bucket" "sbucket-leszek-01" {
   name     = "sbucket-leszek-03"
   location = "europe-north1"
 }
-#GKE cluster
-resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster-03"
-  location = "europe-north1"
-
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
-}
